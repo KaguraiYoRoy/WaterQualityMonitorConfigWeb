069f82f1de89149f15be31f39134b68cc39fefb6
iYoRoy <3363384229@qq.com>
2023-06-15 20:41:17 +0800

    Align

--- index.php
+++ index.php
@@ -11,16 +11,17 @@ if($config != false)
 <body>
 	<h1>OrangePi Water Monitor Config</h1>
 	<form action="register.php" method="post">
-		<sup>*</sup>服务器注册API地址: <input type="text" name="api" <?php if($config != false)echo "value=\"$config->API\""; ?>><br>
-		<sup>*</sup>注册码: <input type="text" name="code" <?php if($config != false)echo "value=\"$config->Code\""; ?>><br>
-		<sup>*</sup>设备代号: <input type="text" name="nick" <?php if($config != false)echo "value=\"$config->Nick\""; ?>><br>
-		UA: <input type="text" name="ua" <?php if($config != false)echo " value=\"$config->UA\""; ?>><br>
-		上传间隔(s): <input type="number" name="interval" <?php if($config != false)echo "value=\"$config->Interval\""; ?>><br>
-		<input name="oled" type="checkbox" value=1 <?php if($config != false)echo $config->UseOled?"checked":"" ?>>使用Oled显示屏<br>
-		<sup>*：必填项<br>其余各项除Oled显示屏开关外留空以使用缺省值</sup><br>
+		<table>
+			<tr><td>服务器注册API地址: </td><td><input type="text" name="api" <?php if($config != false)echo "value=\"$config->API\""; ?>></td></tr>
+			<tr><td>注册码: </td><td><input type="text" name="code" <?php if($config != false)echo "value=\"$config->Code\""; ?>></td></tr>
+			<tr><td>设备代号: </td><td><input type="text" name="nick" <?php if($config != false)echo "value=\"$config->Nick\""; ?>></td></tr>
+			<tr><td>UA<sup>*</sup>: </td><td><input type="text" name="ua" <?php if($config != false)echo " value=\"$config->UA\""; ?>></td></tr>
+			<tr><td>上传间隔(s)<sup>*</sup>: </td><td><input type="number" name="interval" <?php if($config != false)echo "value=\"$config->Interval\""; ?>></td></tr>
+			<tr><td>使用Oled显示屏 </td><td><input name="oled" type="checkbox" value=1 <?php if($config != false)echo $config->UseOled?"checked":"" ?>></td></tr>
+		</table>
+		<sup>*留空以使用缺省值</sup><br>
 		<input type="submit" value="提交">
 	</form>
-
 </body>
 <footer>
 	<p>Copyright. ©2020-2023 iYoRoy Studio.
