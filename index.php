55474be2592543a8f7c4ffd54f18ca744171f849
iYoRoy <3363384229@qq.com>
2023-06-15 20:45:23 +0800

    Change textbox's width

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ if($config != false)
 	<h1>OrangePi Water Monitor Config</h1>
 	<form action="register.php" method="post">
 		<table>
-			<tr><td>服务器注册API地址: </td><td><input type="text" name="api" <?php if($config != false)echo "value=\"$config->API\""; ?>></td></tr>
-			<tr><td>注册码: </td><td><input type="text" name="code" <?php if($config != false)echo "value=\"$config->Code\""; ?>></td></tr>
-			<tr><td>设备代号: </td><td><input type="text" name="nick" <?php if($config != false)echo "value=\"$config->Nick\""; ?>></td></tr>
-			<tr><td>UA<sup>*</sup>: </td><td><input type="text" name="ua" <?php if($config != false)echo " value=\"$config->UA\""; ?>></td></tr>
+			<tr><td>服务器注册API地址: </td><td><input type="text" name="api" style="width:600px" <?php if($config != false)echo "value=\"$config->API\""; ?>></td></tr>
+			<tr><td>注册码: </td><td><input type="text" name="code" style="width:600px" <?php if($config != false)echo "value=\"$config->Code\""; ?>></td></tr>
+			<tr><td>设备代号: </td><td><input type="text" name="nick" style="width:600px" <?php if($config != false)echo "value=\"$config->Nick\""; ?>></td></tr>
+			<tr><td>UA<sup>*</sup>: </td><td><input type="text" name="ua" style="width:600px" <?php if($config != false)echo " value=\"$config->UA\""; ?>></td></tr>
 			<tr><td>上传间隔(s)<sup>*</sup>: </td><td><input type="number" name="interval" <?php if($config != false)echo "value=\"$config->Interval\""; ?>></td></tr>
 			<tr><td>使用Oled显示屏 </td><td><input name="oled" type="checkbox" value=1 <?php if($config != false)echo $config->UseOled?"checked":"" ?>></td></tr>
 		</table>
